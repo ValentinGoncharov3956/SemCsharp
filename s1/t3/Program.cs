3bf614839fc707a854bb35cc73b4660396808b2e
Valik <valik.seal12@gmail.com>
2023-02-21 17:32:46 +0300

    3 task

--- s1/t3/Program.cs
+++ s1/t3/Program.cs
@@ -3,16 +3,43 @@
 // 5 -> Пятница
 
 
-Console.WriteLine("Введите 1 число");
+Console.WriteLine("Введите число от 1 до 7");
 string userAnswer1 = Console.ReadLine();
 int userNumber1 = Convert.ToInt32(userAnswer1);
-Console.WriteLine("Введите 2 число");
-string userAnswer2 = Console.ReadLine();
-int userNumber2 = Convert.ToInt32(userAnswer2);
 
-if (userNumber1 == userNumber2 * userNumber2)
+if (userNumber1 == 1)
 {
-    Console.WriteLine("Да");
+    Console.WriteLine("Понедельник");
 }
-else 
-Console.WriteLine("Нет");
+else
+if (userNumber1 == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else
+if (userNumber1 == 3)
+{
+    Console.WriteLine("Среда");
+}
+else
+if (userNumber1 == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else
+if (userNumber1 == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else
+if (userNumber1 == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else
+if (userNumber1 == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+Console.WriteLine("Ты неправильно выполнил условие:(");
