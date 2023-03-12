d0bbcce8c045af24e39b432d3cd5508e092a8e8e
Valik <valik.seal12@gmail.com>
2023-03-13 02:11:34 +0300

    homework3-2

--- homework/task3/Program.cs
+++ homework/task3/Program.cs
@@ -2,3 +2,17 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите 1 число");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 % 2 == 0) 
+{
+    Console.Write(userNumber1);
+    Console.Write(" - да");
+}
+else 
+{
+    Console.Write(userNumber1);
+    Console.Write(" - нет");
+}
