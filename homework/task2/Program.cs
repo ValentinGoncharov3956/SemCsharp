28146a2462dd33a3c4da6714c3e85b0442b00c93
Valik <valik.seal12@gmail.com>
2023-03-13 01:59:30 +0300

    homework2-2

--- homework/task2/Program.cs
+++ homework/task2/Program.cs
@@ -3,3 +3,23 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+
+Console.WriteLine("Введите 1 число");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+int userNumber3 = Convert.ToInt32(Console.ReadLine());
+int max = userNumber1;
+
+if (userNumber2 > max) 
+{
+    max = userNumber2;
+}
+if (userNumber3 > max)
+{
+    max = userNumber3;
+}
+    Console.Write("max = ");
+    Console.WriteLine(max);
