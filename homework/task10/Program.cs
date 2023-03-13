0e38461156688794ecf387a14cd9fbcf7c9f26b9
Valik <valik.seal12@gmail.com>
2023-03-14 02:17:15 +0300

    homework task 10

---
+++ homework/task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на 
+//вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите любое трёхзначное число");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 < 100 || userNumber1 > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число. Конец Игры."); 
+}
+else
+{
+    Console.WriteLine(userNumber1 / 10 % 10);
+}
