54af9055db2a3fd754f592353f7905042f89bed6
Valik <valik.seal12@gmail.com>
2023-03-14 00:20:57 +0300

    sem2_task2-3

--- s2/t2/Program.cs
+++ s2/t2/Program.cs
@@ -7,5 +7,7 @@ int number = Random.Shared.Next(100, 1000);
 Console.WriteLine(number);
 int lastDigit = number % 10;
 int firstDigit = number / 100;
-Console.Write(firstDigit);
-Console.Write(lastDigit);   //Слишком прсото решение? надо ли усложнять? как?
+// Console.Write(firstDigit);
+// Console.Write(lastDigit);
+int res = firstDigit * 10 + lastDigit;
+Console.Write(res);
