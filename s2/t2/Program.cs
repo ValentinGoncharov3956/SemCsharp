844dfe251c27b0894262740cab2f7683f66f8eb2
Valik <valik.seal12@gmail.com>
2023-03-14 00:12:44 +0300

    sem2_task2-2

--- s2/t2/Program.cs
+++ s2/t2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = Random.Shared.Next(100, 1000);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+int firstDigit = number / 100;
+Console.Write(firstDigit);
+Console.Write(lastDigit);   //Слишком прсото решение? надо ли усложнять? как?
