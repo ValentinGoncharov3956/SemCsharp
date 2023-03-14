5ebdc7d5b0414f86b0984f8216c7d3869f8e4106
Valik <valik.seal12@gmail.com>
2023-03-15 01:05:01 +0300

    s3-t4

---
+++ s3/t4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, 
+// которая принимает на 
+// вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+
+// 5 -> 1, 8, 27, 64, 125.
+// 2 -> 1, 8
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int index =1;
+
+while (index <= n)
+{
+    double a = Math.Pow(index, 3);
+    index++;
+    Console.WriteLine(a);
+}
