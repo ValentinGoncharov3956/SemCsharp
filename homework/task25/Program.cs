dc9d74f1a137f6890ebf83e05a55c00daebf1bf1
Valik <valik.seal12@gmail.com>
2023-03-17 01:40:03 +0300

    homework4-25

--- homework/task25/Program.cs
+++ homework/task25/Program.cs
@@ -4,3 +4,17 @@
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+
+double ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return double.Parse(Console.ReadLine());
+}
+
+double A = ReadNumber("Введите 1 число");
+double B = ReadNumber("Введите 2 число");
+
+double C = Math.Pow(A, B);
+
+Console.WriteLine(C);
