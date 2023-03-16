841d3b61453a261cf3706b18d6714c8b68f64652
Valik <valik.seal12@gmail.com>
2023-03-17 01:26:30 +0300

    task30

---
+++ s4/task30/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 30: Напишите программу, 
+// которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+
+int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+
+int num = ReadNumber("Введите число");
+
+int[] array = new int[num];
+
+for(int i=0; i < num; i++)
+{
+        array[i] = Random.Shared.Next(0,2);
+        Console.Write(array[i] + " ");
+}
