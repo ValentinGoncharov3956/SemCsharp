e6ece04e946f7fc636be6b891a21f5743aba5230
Valik <valik.seal12@gmail.com>
2023-03-17 00:58:01 +0300

    task26

---
+++ s4/task26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 26: Напишите программу, которая 
+// принимает на вход число и выдаёт 
+// количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+
+int Count(int a)
+{
+    int count;
+    for (count = 0; a != 0; count++)
+  {
+    a /= 10;
+  }
+   return count;
+}
+
+int num = ReadNumber("Введите число");
+int result = Count(num);
+Console.WriteLine(result);
