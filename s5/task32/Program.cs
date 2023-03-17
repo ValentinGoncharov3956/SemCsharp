19501e150c91b4528f2be1efa92d276d8a4a4196
Valik <valik.seal12@gmail.com>
2023-03-17 15:32:34 +0300

    sem5-32

--- s5/task32/Program.cs
+++ s5/task32/Program.cs
@@ -4,3 +4,42 @@
 // отрицательные, и наоборот.
 
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+
+int[] RandomArray(int length, int leftRange, int rigthRange)
+{
+    int[] array = new int[length];
+
+    for(int i=0; i < array.Length; i++)
+        {
+            array[i] = Random.Shared.Next(leftRange, rigthRange + 1);
+        }
+    return array;
+}
+
+int[] ReversArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+int lengthArray = ReadNumber("Задайте длину массива");
+int leftBorder = ReadNumber("Задайте левую границу");
+int rightBorder = ReadNumber("Задайте правую границу");
+
+int[] ourArray = RandomArray(lengthArray, leftBorder, rightBorder);
+Console.WriteLine($"[{string.Join(", ", ourArray)}]");
+
+int[] Revers = ReversArray(ourArray);
+Console.WriteLine($"[{string.Join(", ", Revers)}]");
+
+
