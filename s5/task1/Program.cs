ee8e7091a3d3a94320a22f4a614796dfdc11d1ae
Valik <valik.seal12@gmail.com>
2023-03-17 13:37:08 +0300

    sem5

---
+++ s5/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+
+int num = ReadNumber("Введите число");
+
+
+// void Main(string[] args)
+// {
+    int b = 123124;
+    string a = b.ToString();
+// }
+
+Console.WriteLine(a);
---
+++ s5/task31/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 31: Задайте массив из 12 элементов, 
+// заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и 
+// положительных элементов массива.
+
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+// сумма положительных чисел равна 29,
+// сумма отрицательных равна -20.
+
+
+int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+
+int num = ReadNumber("Введите число");
+
+void FillRandomArray(int[] array)
+{
+        for(int i=0; i < num; i++)
+        {
+                array[i] = Random.Shared.Next(0,2);
+        }
+}
+
+int[] array = new int[num];
+FillRandomArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
