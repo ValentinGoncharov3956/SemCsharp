bb080881433db20564c3d8dc6bb661d18112a213
Valik <valik.seal12@gmail.com>
2023-03-17 17:54:42 +0300

    homework s5-task36

--- homework/task36/Program.cs
+++ homework/task36/Program.cs
@@ -4,3 +4,33 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+int lenght = 4;                                         // присваеваем длину массива, через ReadNumber, можно запросить у пользователя любую длинну.
+void RandomArray(int[] array)
+{
+        for(int i=0; i < lenght; i++)
+        {
+                array[i] = Random.Shared.Next(1,100);           // присваеваем диапазон случайных чисел
+        }
+}
+
+int SumElementsArray(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+       if (i % 2 != 0)
+       sum = sum + array[i]; 
+    }
+    return sum;
+}
+
+int[] array = new int[lenght];
+RandomArray(array);
+int sum = SumElementsArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}] -> {sum}");
