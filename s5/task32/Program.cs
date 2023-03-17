7843d4896233bf1b4a87991b64806b27bea888d4
Valik <valik.seal12@gmail.com>
2023-03-17 16:16:11 +0300

    sem5-33

--- s5/task32/Program.cs
+++ s5/task32/Program.cs
@@ -23,13 +23,31 @@ int[] RandomArray(int length, int leftRange, int rigthRange)
     return array;
 }
 
-int[] ReversArray(int[] array)
+// int[] ReversArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] *= -1;
+//     }
+//     return array;
+// }
+
+// int lengthArray = ReadNumber("Задайте длину массива");
+// int leftBorder = ReadNumber("Задайте левую границу");
+// int rightBorder = ReadNumber("Задайте правую границу");
+
+// int[] ourArray = RandomArray(lengthArray, leftBorder, rightBorder);
+// Console.WriteLine($"[{string.Join(", ", ourArray)}]");
+
+// int[] Revers = ReversArray(ourArray);
+// Console.WriteLine($"[{string.Join(", ", Revers)}]");
+
+void ReversArray(int[] array)   // Решение с помощью метода void (обращаемся к массиву как к ссылке)
 {
     for(int i = 0; i < array.Length; i++)
     {
         array[i] *= -1;
     }
-    return array;
 }
 
 int lengthArray = ReadNumber("Задайте длину массива");
@@ -38,8 +56,6 @@ int rightBorder = ReadNumber("Задайте правую границу");
 
 int[] ourArray = RandomArray(lengthArray, leftBorder, rightBorder);
 Console.WriteLine($"[{string.Join(", ", ourArray)}]");
+ReversArray(ourArray);
 
-int[] Revers = ReversArray(ourArray);
-Console.WriteLine($"[{string.Join(", ", Revers)}]");
-
-
+Console.WriteLine($"[{string.Join(", ", ourArray)}]");
--- s5/task33/Program.cs
+++ s5/task33/Program.cs
@@ -4,3 +4,47 @@
 
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
+
+
+int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+
+int[] RandomArray(int length, int leftRange, int rigthRange)
+{
+    int[] array = new int[length];
+
+    for(int i=0; i < array.Length; i++)
+        {
+            array[i] = Random.Shared.Next(leftRange, rigthRange + 1);
+        }
+    return array;
+}
+
+bool Detector(int[] array, int Numb)
+{
+    bool result = false;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (Numb == array[i]) 
+        {
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+int lengthArray = ReadNumber("Задайте длину массива");
+int leftBorder = ReadNumber("Задайте левую границу");
+int rightBorder = ReadNumber("Задайте правую границу");
+int[] ourArray = RandomArray(lengthArray, leftBorder, rightBorder);
+Console.WriteLine($"[{string.Join(", ", ourArray)}]");
+
+int Find = ReadNumber("Введите искомое число");
+bool res = Detector(ourArray, Find);
+Console.WriteLine($"{res}");
+
+
