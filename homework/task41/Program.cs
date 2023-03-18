3c20504e69dbda5876e3bbce4f7b919f98695e03
Valik <valik.seal12@gmail.com>
2023-03-18 23:47:00 +0300

    s6 homework - done

--- homework/task41/Program.cs
+++ homework/task41/Program.cs
@@ -3,3 +3,38 @@
 
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
+
+int ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return int.Parse(Console.ReadLine());
+}
+
+int[] ArrayUser(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadNumber($"Введите число");
+    }
+    return array;
+}
+
+int Sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+        sum = sum + 1;
+        }
+    }
+    return sum;
+}
+
+int lenght = ReadNumber("Введите длину массива");
+int[] array;
+array = ArrayUser(lenght);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Количество чисел больше 0 - {Sum(array)}");
--- homework/task43/Program.cs
+++ homework/task43/Program.cs
@@ -1,6 +1,22 @@
-﻿// Задача 43. Напишите программу, которая найдёт точку 
+﻿// Задача 43. Напишите программу, которая найдёт координату 
 // пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double ReadNumber(string message)
+{
+        Console.WriteLine(message);
+        return double.Parse(Console.ReadLine());
+}
+
+double b1 = ReadNumber("Введите первую координату по X");
+double k1 = ReadNumber("Введите вторую координату по X");
+double b2 = ReadNumber("Введите первую координату по Y");
+double k2 = ReadNumber("Введите вторую координату по Y");
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
