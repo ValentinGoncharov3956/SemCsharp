b423ba6da025b1b5b98abb1f856f633e709faba9
Valik <valik.seal12@gmail.com>
2023-03-20 00:56:08 +0300

    homework task 52

--- homework/task52/Program.cs
+++ homework/task52/Program.cs
@@ -8,3 +8,57 @@
 // 8 4 2 4
 
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows = 5, int colums = 5, int leftBorder = 0, int rightBorder = 10)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + ", ");
+        }
+        Console.WriteLine("]");
+    }
+    Console.WriteLine();
+}
+
+void Average(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            average = (average + matrix[i, j]);
+        }
+        average = average / matrix.GetLength(0);
+        Console.Write($"[{average}]");
+    }
+}
+
+int[,] myMatrix = GetRandomMatrix();
+PrintMatrix(myMatrix);
+Console.Write($"Среднее арифметическое каждого столбца: ");
+Average(myMatrix);
+
