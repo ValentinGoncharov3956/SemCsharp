c829ce90a2a3325344435cb4e8d63e5b572c0709
Valik <valik.seal12@gmail.com>
2023-03-20 21:06:41 +0300

    sem8 task53, task55

---
+++ s8/task53/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows = 5, int colums = 5, int leftBorder = 0, int rightBorder = 10)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + ", ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void Exchange(int[,] matrix)
+{
+     for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = temp;
+
+    }
+}
+
+int[,] myMatrix = GetRandomMatrix();
+PrintMatrix(myMatrix);
+Exchange(myMatrix);
+Console.WriteLine("Новый массив:");
+PrintMatrix(myMatrix);
---
+++ s8/task55/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, если это 
+// невозможно, программа должна вывести сообщение для пользователя.
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows = 8, int colums = 3, int leftBorder = 0, int rightBorder = 10)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] NewMatrix(int[,] matrix)
+{
+    int[,] newmatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            newmatrix[i, j] = matrix[j, i];
+        }
+    }
+    return newmatrix;
+}
+
+void PrintMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + ", ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] myMatrix = GetRandomMatrix();
+PrintMatrix(myMatrix);
+Console.WriteLine();
+Console.WriteLine("Новый массив:");
+int[,] newMatrix = NewMatrix(myMatrix);
+PrintMatrix(newMatrix);
