848600943e84967d81eeedd90c1e45044932ed84
Valik <valik.seal12@gmail.com>
2023-03-23 00:05:34 +0300

    homework task 54

---
+++ homework/task54/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 54: Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по возрастанию 
+//элементы каждой строки двумерного массива.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+
+// 1 2 4 7
+
+// 2 3 5 9
+
+// 2 4 4 8
+
+int[,] GetRandomMatrix(int rows = 3, int colums = 10, int leftBorder = 0, int rightBorder = 10)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + ", ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void NewMatrix(int[,] matrix)
+{
+    for (int k = 1; k < matrix.GetLength(1); k++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] > matrix[i, j + 1])       // если знак (<) меняем на сортировку по убыванию 
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+int[,] myMatrix = GetRandomMatrix();
+PrintMatrix(myMatrix);
+NewMatrix(myMatrix);
+Console.WriteLine("Новый массив:");
+PrintMatrix(myMatrix);
---
+++ homework/task56/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с 
+// наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой 
+// строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] GetRandomMatrix(int rows = 4, int colums = 5, int leftBorder = 0, int rightBorder = 10)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + ", ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void FindLeast(int[,] matrix)
+{
+    int sum = 0;
+    int minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i,j];
+        }
+        Console.WriteLine($"{sum}");
+    }
+    if(minsum < sum)
+    {
+        minsum = sum;
+        System.Console.WriteLine($"строка с наименьшой суммой элементов{minsum}");
+    }
+}
+
+int[,] myMatrix = GetRandomMatrix();
+PrintMatrix(myMatrix);
+Console.WriteLine("Новый массив:");
+FindLeast(myMatrix);
