80ea2dc179d55ba0d5f9794adbf7818cabef08f4
Valik <valik.seal12@gmail.com>
2023-03-24 17:45:39 +0300

    homework task 62

--- homework/task62/Program.cs
+++ homework/task62/Program.cs
@@ -9,3 +9,44 @@
 // 11 16 15 6
 
 // 10 9 8 7
+
+int[,] GetRandomMatrix(int rows = 4, int colums = 4)
+{
+    int[,] matrix = new int[rows, colums];
+
+    int number = 1;
+    int i = 0;
+    int j = 0;
+    while (number <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = number;
+        number++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else 
+        if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else 
+        if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + ", ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] myMatrix = GetRandomMatrix();
+PrintMatrix(myMatrix);
