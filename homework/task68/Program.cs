77a24fc63db4d08535850d826197548214026765
Valik <valik.seal12@gmail.com>
2023-03-25 21:11:08 +0300

    homework task68

--- homework/task68/Program.cs
+++ homework/task68/Program.cs
@@ -2,3 +2,17 @@
 // Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
 // m = 2, n = 3 -> A(m,n) = 9
+
+int M = 3;
+int N = 3;
+
+int FunctionAckermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return FunctionAckermann(m - 1, 1);
+    if (m > 0 && n > 0) return FunctionAckermann(m - 1, FunctionAckermann(m, n - 1));
+    return FunctionAckermann(m,n);
+}
+
+int Function = FunctionAckermann(M, N);
+Console.WriteLine(Function);
