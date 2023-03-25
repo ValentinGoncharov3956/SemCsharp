1cc0205824ea5de7f93115e1789633ae660f8174
Valik <valik.seal12@gmail.com>
2023-03-25 20:47:10 +0300

    homework task64

--- homework/task64/Program.cs
+++ homework/task64/Program.cs
@@ -4,3 +4,15 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+uint number = 8;
+
+void NaturalNUmber(uint n)
+{
+    if (n == 0) return;
+    Console.Write($"{n} ");
+    NaturalNUmber(n-1);
+}
+
+NaturalNUmber(number);
+
